7ba40856cc2fee153628abc6f9846ee2a0a29286
MIV <imivi98@gmail.com>
2024-01-17 20:32:23 +0100

    Create Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
 public class Server {
+
+    public static final String IP_SERVER = "192.168.0.1";
+    public static final int PORT = 5;
+
+    private ArrayList<String> usuariosConectados = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+
     }
 }
