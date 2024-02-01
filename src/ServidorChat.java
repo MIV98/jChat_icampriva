f2520913fd81790bb922190125acab145dc8bd3c
MIV <imivi98@gmail.com>
2024-02-01 16:46:37 +0100

    Some synchronization (might not be needed) and change usuariosConectados access modifier to public

--- src/ServidorChat.java
+++ src/ServidorChat.java
@@ -12,7 +12,7 @@ public class ServidorChat {
     public static final String IP_SERVER = "192.168.0.27";
     public static final int PORT = 5;
 
-    private static HashMap<String, ClienteThread> usuariosConectados = new HashMap<>();
+    public static HashMap<String, ClienteThread> usuariosConectados = new HashMap<>();
     private static ServerSocket server;
 
     public enum Comando {
@@ -36,7 +36,15 @@ public class ServidorChat {
                 System.out.println(nick + "\t" + cliente.getInetAddress() + "\tCONECTADO");
 
                 ClienteThread cli = new ClienteThread(cliente, nick);
-                usuariosConectados.put(nick, cli);
+                
+                // Send the client their Thread nickname so they can acces it and wait for it to complete
+                cliente.getOutputStream().write(nick.getBytes());
+
+                // Do I need this?
+                synchronized (usuariosConectados) {
+                    usuariosConectados.put(nick, cli);
+                }
+                
                 cli.start();
             }
 
@@ -57,23 +65,27 @@ public class ServidorChat {
                         "conversación o responder a un usuario utilza el comando" +
                         " #charlar <nic>").getBytes());
             } else if(!comando.startsWith("#") && cliente.isConversando()) {
-                // TODO check if receptor is still connected and end conversation if they're not
-                ClienteThread receptor = usuariosConectados.get(cliente.getNickReceptor());
-                String mensaje = ">" + cliente.getNick() + " " + comando;
-                receptor.getSocket().getOutputStream().write(mensaje.getBytes());
-            }else {
+                synchronized (usuariosConectados) {
+                    // TODO check if receptor is still connected and end conversation if they're not
+                    ClienteThread receptor = usuariosConectados.get(cliente.getNickReceptor());
+                    String mensaje = ">" + cliente.getNick() + " " + comando;
+                    receptor.getSocket().getOutputStream().write(mensaje.getBytes());
+                }
+            } else {
                 comando = comando.substring(1); // skip the "#"
 
                 // TODO refactor to a SWITCH
                 if (comando.contains(Comando.CHARLAR.toString())) {
-                    String[] split = comando.split(" ");
-                    if (!usuariosConectados.containsKey(split[1])) {
-                        cliente.getSocket().getOutputStream().write(("[ERROR] " +
-                        "El usuario" + split[1] + " no se encuentra conectado." +
-                        " Utiliza el comando #list para ver los usuarios " +
-                        "conectados").getBytes());
-                    } else {
-                        cliente.iniciarConversacion(split[1]);
+                    synchronized (usuariosConectados) {
+                        String[] split = comando.split(" ");
+                        if (!usuariosConectados.containsKey(split[1])) {
+                            cliente.getSocket().getOutputStream().write(("[ERROR] " +
+                            "El usuario" + split[1] + " no se encuentra conectado." +
+                            " Utiliza el comando #list para ver los usuarios " +
+                            "conectados").getBytes());
+                        } else {
+                            cliente.iniciarConversacion(split[1]);
+                        }
                     }
                 } else if(comando.contains(Comando.AYUDA.toString())) {
                     cliente.getSocket().getOutputStream().write(("#listar: lista" +
@@ -81,11 +93,13 @@ public class ServidorChat {
                         "#charlar <usuario>: comienza la comunicación con el usuario <usuario>\n" + 
                         "#salir: se desconecta del chat").getBytes());
                 } else if (comando.contains(Comando.LISTAR.toString())) {
-                    String salida = "Actualmente están conectados " 
-                        + usuariosConectados.size() + " usuarios:\n";
-
-                    for (Entry<String, ClienteThread> e : usuariosConectados.entrySet()) {
-                        salida += e.getKey() + "\n"; // the key is the username
+                    synchronized (usuariosConectados) {
+                        String salida = "Actualmente están conectados " 
+                            + usuariosConectados.size() + " usuarios:\n";
+    
+                        for (Entry<String, ClienteThread> e : usuariosConectados.entrySet()) {
+                            salida += e.getKey() + "\n"; // the key is the username
+                        }
                     }
 
                     cliente.getSocket().getOutputStream().write(salida.getBytes());
@@ -105,13 +119,14 @@ public class ServidorChat {
     }
 
     public static void desconectarCliente(ClienteThread cliente) {
-        if (usuariosConectados.containsKey(cliente.getNick())) {
-            System.out.println(cliente.getNick() + "\t" 
-            + cliente.getSocket().getInetAddress() + "\tDESCONECTADO");
-
-            usuariosConectados.get(cliente.getNick()).interrupt();
-            usuariosConectados.remove(cliente.getNick());
+        synchronized (usuariosConectados) {
+            if (usuariosConectados.containsKey(cliente.getNick())) {
+                System.out.println(cliente.getNick() + "\t" 
+                + cliente.getSocket().getInetAddress() + "\tDESCONECTADO");
+    
+                usuariosConectados.get(cliente.getNick()).interrupt();
+                usuariosConectados.remove(cliente.getNick());
+            }
         }
-        
     }
 }
